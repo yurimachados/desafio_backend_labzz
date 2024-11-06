86354d241905f64e271d7ca76cbefb784707ee08
Yuri Machado <ymachado1995@gmail.com>
2024-11-06 20:39:06 -0300

    feat: rotas de crud de usuário

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,40 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const router = express.Router();
+
+/**
+ * @route POST /users
+ * @desc Cria um novo usuário
+ * @access Público
+ */
+router.post('/users', userController.createUser);
+
+/**
+ * @route GET /users
+ * @desc Retorna todos os usuários
+ * @access Público
+ */
+router.get('/users', userController.getAllUsers);
+
+/**
+ * @route GET /users/:id
+ * @desc Retorna um usuário pelo ID
+ * @access Público
+ */
+router.get('/users/:id', userController.getUserById);
+
+/**
+ * @route PUT /users/:id
+ * @desc Atualiza um usuário pelo ID
+ * @access Público
+ */
+router.put('/users/:id', userController.updateUser);
+
+/**
+ * @route DELETE /users/:id
+ * @desc Deleta um usuário pelo ID
+ * @access Público
+ */
+router.delete('/users/:id', userController.deleteUser);
+
+module.exports = router;
