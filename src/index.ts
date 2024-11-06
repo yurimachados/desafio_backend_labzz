a466de2010703c7fc44d3b2c63eb943d1f02d280
Yuri Machado <ymachado1995@gmail.com>
2024-11-06 17:54:27 -0300

    feat: geração dos modelos base para a aplicação

--- src/index.ts
+++ src/index.ts
@@ -34,16 +34,6 @@ app.get('/users', async (req, res) => {
     res.json(users);
 });
 
-app.post('/users', async (req, res) => {
-    const { name, email } = req.body;
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-        },
-    });
-    res.json(user);
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
