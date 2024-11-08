78f7396a99cd5275f5efc77c36f2f035fe13bfc6
Yuri Machado <ymachado1995@gmail.com>
2024-11-07 23:38:34 -0300

    fix: remover configurações desnecessárias do Jest

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/singleton.ts'],
-  testLocationInResults: true,
-  reporters: [
-    'default',
-    '/home/yuri/.vscode-server/extensions/orta.vscode-jest-6.4.0/out/reporter.js',
-  ],
 };
