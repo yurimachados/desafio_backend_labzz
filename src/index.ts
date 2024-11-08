00fb88301f1ab99ffebc36d483522fab6f16ca36
Yuri Machado <ymachado1995@gmail.com>
2024-11-08 15:53:23 -0300

    feat: adicionar suporte a variáveis de ambiente para desenvolvimento e ajustar configuração do package.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 
+import dotenv from 'dotenv';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: path.resolve(__dirname, '../.env.development') });
+} else {
+  dotenv.config({ path: path.resolve(__dirname, '../.env') });
+}
+
 const app = express();
 app.use(express.json());
 app.use('/api', userRoutes);
