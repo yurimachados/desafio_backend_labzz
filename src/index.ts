0d88e686b3f3a3ebbb6f79e0870635b7736d52be
Yuri Machado <ymachado1995@gmail.com>
2024-11-08 18:48:13 -0300

    feat: implementação de helmet na aplicação
    abstração do app, para melhor organização de imports

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import helmet from 'helmet';
-
-// Carregar variáveis de ambiente com base no NODE_ENV
-const envPath =
-  process.env.NODE_ENV === 'development'
-    ? path.resolve(__dirname, '../.env.development')
-    : path.resolve(__dirname, '../.env');
-
-dotenv.config({ path: envPath });
-
-import express from 'express';
-import userRoutes from './routes/userRoutes';
-
-const app = express();
-
-app.use(helmet());
-app.use(express.json());
-app.use('/api', userRoutes);
+import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-console.log(`Node Environment: ${process.env.NODE_ENV}`);
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
