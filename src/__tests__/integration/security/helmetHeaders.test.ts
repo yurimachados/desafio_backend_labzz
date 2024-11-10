b41d39aa5b34577471ef86375455c84eefdd5938
Yuri Machado <ymachado1995@gmail.com>
2024-11-09 23:44:56 -0300

    feat: renomear teste de segurança do Helmet e ajustar caminho de importação

--- src/__tests__/integration/security/helmetHeaders.test.ts
+++ src/__tests__/integration/security/helmetHeaders.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../app';
+import app from '../../../app';
 
 describe('Testando Helmet', () => {
   it('Deve retornar os cabeçalhos de segurança do Helmet', async () => {
