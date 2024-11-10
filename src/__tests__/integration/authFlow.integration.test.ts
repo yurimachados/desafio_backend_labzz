61640486255e8fb81c987af7a12f4dd276cbb6b5
Yuri Machado <ymachado1995@gmail.com>
2024-11-10 04:04:04 -0300

    feat: adicionar testes de integração para fluxo de autenticação, incluindo validações, sessão, csrf e cors

--- src/__tests__/integration/authFlow.integration.test.ts
+++ src/__tests__/integration/authFlow.integration.test.ts
@@ -1,88 +1,210 @@
 import request from 'supertest';
-import app from '../../app'; // Assumindo que o app Express está sendo exportado de um arquivo app.ts ou similar
+import app from '../../app';
 import { faker } from '@faker-js/faker';
+import { findUserByEmail, deleteUser } from '../../services/userService';
 
 describe('Auth Flow Integration Tests', () => {
-  beforeAll(async () => {
-    const testUser = {
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      username: faker.internet.userName(),
-    };
-  });
+  const testUser = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    username: faker.internet.username(),
+  };
+
+  let csrfToken: string;
+  let accessTokenCookie: string;
 
   afterAll(async () => {
-    // Limpeza da base de dados após todos os testes
+    findUserByEmail(testUser.email).then(async (user) => {
+      if (user) {
+        await deleteUser(user.id);
+      }
+    });
   });
 
   describe('POST /register', () => {
     it('should register the faker user on database', async () => {
-      // Implementação do teste
+      const response = await request(app)
+        .post('/api/auth/register')
+        .send(testUser)
+        .expect(201);
+
+      expect(response.body.message).toBe('Register successful');
+      expect(response.headers).toHaveProperty('x-csrf-token');
+      expect(response.headers).toHaveProperty('set-cookie');
+
+      csrfToken = response.headers['x-csrf-token'];
+      const cookies = response.headers['set-cookie'] as unknown as string[];
+      accessTokenCookie = cookies.find((cookie: string) =>
+        cookie.startsWith('access_token'),
+      ) as string;
     });
 
-    it('should ', async () => {
-      // Implementação do teste
+    it('should return 400 when trying to register with an already used email', async () => {
+      const response = await request(app)
+        .post('/api/auth/register')
+        .send(testUser)
+        .expect(400);
+
+      expect(response.body.message).toBe('Email already in use');
+    });
+
+    it('should return 400 when trying to register with invalid email, username and password', async () => {
+      const response = await request(app)
+        .post('/api/auth/register')
+        .send({
+          username: null,
+          password: faker.string.alphanumeric(5),
+          email: 'invalid-email',
+        })
+        .expect(400);
+
+      expect(response.body.errors).toEqual(
+        expect.arrayContaining([
+          'Invalid email',
+          'Username is required',
+          'Password should be at least 6 characters',
+        ]),
+      );
+    });
+
+    it('should return 400 when trying to register with an active session', async () => {
+      const response = await request(app)
+        .post('/api/auth/register')
+        .set('Cookie', accessTokenCookie)
+        .send({
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+          username: faker.internet.username(),
+        })
+        .expect(400);
+
+      expect(response.body.error).toBe('You are already in an active session');
     });
   });
 
   describe('POST /login', () => {
     it('should login user and return JWT token', async () => {
-      // Implementação do teste
+      let loginPayload = {
+        email: testUser.email,
+        password: testUser.password,
+      };
+      const response = await request(app)
+        .post('/api/auth/login')
+        .send(loginPayload)
+        .expect(200);
+
+      expect(response.body.message).toBe('Login successful');
+      expect(response.headers).toHaveProperty('set-cookie');
     });
 
     it('should return 401 for invalid credentials', async () => {
-      // Implementação do teste
+      const response = await request(app)
+        .post('/api/auth/login')
+        .send({
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+        })
+        .expect(401);
+
+      expect(response.body.message).toBe('Invalid credentials');
+    });
+
+    it('should return 400 for invalid email and password', async () => {
+      const response = await request(app)
+        .post('/api/auth/login')
+        .send({
+          email: 'invalid-email',
+          password: faker.string.alphanumeric(5),
+        })
+        .expect(400);
+
+      expect(response.body.errors).toEqual(
+        expect.arrayContaining([
+          'Invalid email',
+          'Password should be at least 6 characters',
+        ]),
+      );
+    });
+
+    it('should return 400 if user is already in an active session', async () => {
+      const response = await request(app)
+        .post('/api/auth/login')
+        .set('Cookie', accessTokenCookie)
+        .send({
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+        })
+        .expect(400);
+
+      expect(response.body.error).toBe('You are already in an active session');
     });
   });
 
   describe('POST /logout', () => {
     it('should logout user and invalidate session', async () => {
-      // Implementação do teste
+      const response = await request(app)
+        .post('/api/auth/logout')
+        .set('Cookie', accessTokenCookie)
+        .set('x-csrf-token', csrfToken)
+        .expect(200);
+
+      expect(response.body.message).toBe('Logout successful');
+      expect(response.headers['set-cookie']).toEqual(
+        expect.arrayContaining([
+          expect.stringContaining(
+            'access_token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+          ),
+        ]),
+      );
+    });
+
+    it('should return 400 if access token is not found', async () => {
+      const response = await request(app).post('/api/auth/logout').expect(401);
+
+      expect(response.body.message).toBe('Access token is missing');
+    });
+
+    it('should return 400 if CSRF token is not found', async () => {
+      const response = await request(app)
+        .post('/api/auth/logout')
+        .set('Cookie', accessTokenCookie)
+        .expect(403);
+
+      expect(response.body.message).toBe('Invalid CSRF token');
     });
 
     it('should return 401 if user is not authenticated', async () => {
-      // Implementação do teste
+      const response = await request(app).post('/api/auth/logout').expect(401);
+
+      expect(response.body.message).toBe('Access token is missing');
     });
   });
 
-  // Descomente e implemente os testes abaixo se as rotas OAuth2 estiverem habilitadas
-  // describe('GET /oauth2', () => {
-  //   it('should redirect to OAuth2 provider for authentication', async () => {
-  //     // Implementação do teste
-  //   });
-  // });
+  describe('CORS', () => {
+    it('should have CORS headers for allowed origins', async () => {
+      const response = await request(app)
+        .options('/api/auth/register')
+        .set('Origin', 'http://localhost:3000')
+        .expect(204);
 
-  // describe('GET /oauth2/callback', () => {
-  //   it('should authenticate user with OAuth2 provider and return a JWT token', async () => {
-  //     // Implementação do teste
-  //   });
+      expect(response.headers).toHaveProperty(
+        'access-control-allow-origin',
+        'http://localhost:3000',
+      );
+      expect(response.headers).toHaveProperty('access-control-allow-methods');
+      expect(response.headers).toHaveProperty(
+        'access-control-allow-credentials',
+        'true',
+      );
+    });
 
-  //   it('should return 401 if OAuth2 authentication fails', async () => {
-  //     // Implementação do teste
-  //   });
-  // });
+    it('should not allow CORS for disallowed origins', async () => {
+      const response = await request(app)
+        .options('/api/auth/register')
+        .set('Origin', 'http://disallowed-origin.com')
+        .expect(204);
 
-  // describe('POST /2fa/setup', () => {
-  //   it('should generate and return 2FA secret for user', async () => {
-  //     // Implementação do teste
-  //   });
-
-  //   it('should return 401 if user is not authenticated', async () => {
-  //     // Implementação do teste
-  //   });
-  // });
-
-  // describe('POST /2fa/verify', () => {
-  //   it('should verify 2FA token and return success', async () => {
-  //     // Implementação do teste
-  //   });
-
-  //   it('should return 400 for invalid 2FA token', async () => {
-  //     // Implementação do teste
-  //   });
-
-  //   it('should return 401 if user is not authenticated', async () => {
-  //     // Implementação do teste
-  //   });
-  // });
+      expect(response.headers['access-control-allow-origin']).toBeUndefined();
+    });
+  });
 });
